e075a2e17f9c60a7c891b6bc9461eafe64cc7738
MD ABU HASAN <abuhasan43098@gmail.com>
2024-12-27 21:41:59 +0600

    Create node.js
    Hello World!

---
+++ node.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer(function(req, res) {
+    res.end('Hello World!');
+});
+server.listen(5050, () => {
+    console.log("Server started on port 5050");
+});
