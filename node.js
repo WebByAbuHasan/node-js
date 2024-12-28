ca35f940964a7ffdef271569afc9ccc7f32bc8a8
MD ABU HASAN <abuhasan43098@gmail.com>
2024-12-28 20:46:41 +0600

    recqust responce url

--- node.js
+++ node.js
@@ -1,6 +1,18 @@
 const http = require('http');
 const server = http.createServer(function(req, res) {
-    res.end('Hello World!');
+   if(req.url==="/"){
+       res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+       res.write(`<h1>hello this is my fast program</h1>`)
+   }
+    else if(req.url==="/about"){
+       res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+       res.write(`<h1>hello this is my about program</h1>`)
+    }
+   else if(req.url==="/contact"){
+       res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+       res.write(`<h1>hello this is my contact program</h1>`)
+   }
+
 });
 server.listen(5050, () => {
     console.log("Server started on port 5050");
